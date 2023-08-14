823c5ede92d0efcf57d2270f538d92bbe564da7d
Ivan Golubic <ivan@iotaap.io>
2023-08-14 13:49:22 +0200

    Implemented logging

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,43 @@ import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 import validationOptions from './utils/validation-options';
 import { AllConfigType } from './config/config.type';
+import { WinstonModule } from 'nest-winston';
+import { format, transports } from 'winston';
+import 'winston-daily-rotate-file';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: WinstonModule.createLogger({
+      transports: [
+        new transports.DailyRotateFile({
+          filename: `logs/%DATE%-error.log`,
+          level: 'error',
+          format: format.combine(format.timestamp(), format.json()),
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: false,
+          maxFiles: '30d', // keep logs for 30 days
+        }),
+        new transports.DailyRotateFile({
+          filename: `logs/%DATE%-combined.log`,
+          format: format.combine(format.timestamp(), format.json()),
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: false,
+          maxFiles: '30d', // keep logs for 30 days
+        }),
+        new transports.Console({
+          format: format.combine(
+            format.cli(),
+            format.splat(),
+            format.timestamp(),
+            format.printf((info) => {
+              return `${info.timestamp} ${info.level}: ${info.message}`;
+            }),
+          ),
+        }),
+      ],
+    }),
+  });
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   const configService = app.get(ConfigService<AllConfigType>);
 
