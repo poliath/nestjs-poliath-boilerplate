69e53b0bcc558ce35860740417a2de6af4c0b8f3
Ivan Golubic <ivan@iotaap.io>
2023-08-14 18:09:12 +0200

    Fixed migration

--- src/database/migrations/1691769417263-Articles.ts
+++ src/database/migrations/1691769417263-Articles.ts
@@ -5,7 +5,7 @@ export class Articles1691769417263 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "article" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "published" boolean NOT NULL DEFAULT true, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "editedAt" TIMESTAMP, "content" text NOT NULL, "authorId" integer, CONSTRAINT "PK_40808690eb7b915046558c0f81b" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "article" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "published" boolean NOT NULL DEFAULT true, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "editedAt" TIMESTAMP NOT NULL DEFAULT now(), "content" text NOT NULL, "authorId" integer, CONSTRAINT "PK_40808690eb7b915046558c0f81b" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `ALTER TABLE "article" ADD CONSTRAINT "FK_a9c5f4ec6cceb1604b4a3c84c87" FOREIGN KEY ("authorId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
